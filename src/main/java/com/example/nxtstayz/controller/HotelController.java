7741f5e3079126903b69899c516088a13bc1f893
Kuldeep Nimawat <99901894+kd-nimawat08@users.noreply.github.com>
2024-04-23 15:10:19 +0530

    HotelController.java

--- src/main/java/com/example/nxtstayz/controller/HotelController.java
+++ src/main/java/com/example/nxtstayz/controller/HotelController.java
@@ -1,39 +1,45 @@
 package com.example.nxtstayz.controller;
 
+import com.example.nxtstayz.model.Room;
 import com.example.nxtstayz.model.Hotel;
-import com.example.nxtstayz.service.HotelJpaService;
+import com.example.nxtstayz.service.RoomJpaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
-public class HotelController {
+public class RoomController {
     @Autowired
-    private HotelJpaService hotelJpaService;
+    private RoomJpaService roomJpaService;
 
-    @GetMapping("/hotels")
-    public ArrayList<Hotel> getHotels() {
-        return hotelJpaService.getHotels();
+    @GetMapping("/hotels/rooms")
+    public ArrayList<Room> getRooms() {
+        return roomJpaService.getRooms();
     }
 
-    @GetMapping("/hotels/{hotelId}")
-    public Hotel getHotelById(@PathVariable("hotelId") int hotelId) {
-        return hotelJpaService.getHotelById(hotelId);
+    @GetMapping("/hotels/rooms/{roomId}")
+    public Room getRoomById(@PathVariable("roomId") int roomId) {
+        return roomJpaService.getRoomById(roomId);
     }
 
-    @PostMapping("/hotels")
-    public Hotel addHotel(@RequestBody Hotel hotel) {
-        return hotelJpaService.addHotel(hotel);
+    @PostMapping("/hotels/rooms")
+    public Room addRoom(@RequestBody Room room) {
+        return roomJpaService.addRoom(room);
     }
 
-    @PutMapping("/hotels/{hotelId}")
-    public Hotel updateHotel(@PathVariable("hotelId") int hotelId, @RequestBody Hotel hotel) {
-        return hotelJpaService.updateHotel(hotelId, hotel);
+    @PutMapping("/hotels/rooms/{roomId}")
+    public Room updateRoom(@PathVariable("roomId") int roomId, @RequestBody Room room) {
+        return roomJpaService.updateRoom(roomId, room);
     }
 
-    @DeleteMapping("/hotels/{hotelId}")
-    public void deleteHotel(@PathVariable int hotelId) {
-        hotelJpaService.deleteHotel(hotelId);
+    @DeleteMapping("/hotels/rooms/{roomId}")
+    public void deleteRoom(@PathVariable int roomId) {
+        roomJpaService.deleteRoom(roomId);
+    }
+
+    @GetMapping("/rooms/{roomId}/hotel")
+    public Hotel getRoomHotel(@PathVariable int roomId) {
+        return roomJpaService.getRoomHotel(roomId);
     }
 }
